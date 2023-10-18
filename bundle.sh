d7d5590e3d8557cf9142ff9fe78e4fd887876b89
Richard Snider <lighthousemaniac@gmail.com>
2023-10-18 14:13:24 -0600

    update bundle script to use current format

--- bundle.sh
+++ bundle.sh
@@ -4,4 +4,4 @@ version="$(jq -r <ccmod.json .version)";
 filename="CCMultiworldRandomizer-$version.ccmod";
 
 [ -f "$filename" ] && rm "$filename"
-zip -r "$filename" assets ccmod.json data/data.json plugin.js
+zip -r "$filename" assets ccmod.json data/out mw-rando
